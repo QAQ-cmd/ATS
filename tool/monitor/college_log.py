e6e12c487f0f870bc46c242e196f96397531db0f
yangxu <yangxu@hopelead.com>
2022-04-20 10:10:35 +0800

    基本的rs232端口配置脚本OK

--- tool/monitor/college_log.py
+++ tool/monitor/college_log.py
@@ -12,12 +12,19 @@ log_path = log_dir() + '\\run_log.txt'
 temp_path = temp_dir() + '\\COM.txt'
 
 
-def collect_log_mf(log):
-    print(log)
-    str(log)
+def collect_log_mf(*log):
+    """
+    传入未定参数，出来是个元组，用for循环搞他
+    """
+    temp_b = ''
+    for i in log:
+        str(i)
+        temp_b += i
+    print(temp_b)
     with open(log_path, 'a', encoding='utf8') as temp:
         temp.write(now_time_exact + '\n')
-        temp.write(log + '\n')
+        temp.write(temp_b)
+        temp.write('\n')
 
 
 if __name__ == "__main__":
